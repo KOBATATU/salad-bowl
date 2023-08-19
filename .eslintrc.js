e6ab802b45834694550aff4a62eb3fc428b7548c
KOBATATU <kobatatu258@yahoo.co.jp>
2023-08-19 13:38:19 +0900

    update: eslintの更新とそれに伴うファイルの修正

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  "extends": [
+    "next/core-web-vitals",
+    "prettier",
+    'plugin:tailwindcss/recommended',
+  ],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions":  {
+    "project": "tsconfig.json"
+  },
+  "plugins": ["@typescript-eslint", 'tailwindcss'],
+  "root": true,
+  rules: {
+    'react/jsx-indent': ['error', 2, { indentLogicalExpressions: true }],
+    "react/jsx-indent-props": ['error', 2],
+    indent: ['error', 2],
+  },
+}
