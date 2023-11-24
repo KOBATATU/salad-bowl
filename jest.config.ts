1f8a95a196a065c46c995b3f42711a4b4bd47ae7
KOBATATU <kobatatu258@yahoo.co.jp>
2023-11-24 20:45:49 +0900

    add: jestの設定にreportsを追加した

--- jest.config.ts
+++ jest.config.ts
@@ -5,7 +5,17 @@ const customJestConfig = {
   testPathIgnorePatterns: [ '<rootDir>/.next/', '<rootDir>/node_modules/'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleDirectories: ['node_modules', '<rootDir>/src'],
-  testEnvironment: 'jsdom'
+  testEnvironment: 'jsdom',
+  reporters: [
+    'default',
+    [
+      'jest-html-reporters',
+      {
+        publicPath: '__reports__',
+        filename: 'jest.html',
+      },
+    ],
+  ],
 }
 
 module.exports = createJestConfig(customJestConfig)
