e72498baa930925087d36c5c086b0edaa4609580
KOBATATU <kobatatu258@yahoo.co.jp>
2023-11-25 22:04:53 +0900

    update: apiの呼び出しができるまで待機させる

--- src/domain/pokemon/__mock__/mockPokemon.ts
+++ src/domain/pokemon/__mock__/mockPokemon.ts
@@ -1,5 +1,3 @@
 export const mockPokemonPickachu = {
-  data :{
-    id: 25
-  }
+  id: 25
 }
--- src/features/pokemon/[pokemon]/components/Container/__test__/TestPokemonContainer.test.tsx
+++ src/features/pokemon/[pokemon]/components/Container/__test__/TestPokemonContainer.test.tsx
@@ -1,13 +1,17 @@
-import { QueryClient } from '@tanstack/react-query'
-import { render } from '@testing-library/react'
-import { QueryClientProviders } from '@/components/QueryClientProviders'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { screen } from '@testing-library/dom'
+import { render, waitFor } from '@testing-library/react'
 import { PokemonContainer } from '@/features/pokemon/[pokemon]/components/Container/PokemonContainer'
 
 const queryClient = new QueryClient()
 describe('レンダリングテスト', () => {
-  test('APIをhook経由で呼び出せるか', () =>{
-    render(<QueryClientProviders>
+  test('APIをhook経由で呼び出せるか', async () =>{
+    render(<QueryClientProvider client={queryClient}>
       <PokemonContainer params={{ pokemonName: 'pickachu' }} />
-    </QueryClientProviders>)
+    </QueryClientProvider>)
+    await waitFor(() => {
+      expect(screen.getByText('25')).toBeInTheDocument()
+    })
+    screen.debug()
   })
 })
