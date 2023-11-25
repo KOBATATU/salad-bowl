2de5156aa204f8751647d0234d0e56bfbfb3ad78
KOBATATU <kobatatu258@yahoo.co.jp>
2023-11-25 22:04:30 +0900

    update: hostnameを環境変数として定義

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/proxy/pokemon/:match*',
+        destination: `${process.env.API_POKEMON}/:match*`
+      }
+    ]
+  }
+}
 
 module.exports = nextConfig
--- src/domain/pokemon/__mock__/handlers.ts
+++ src/domain/pokemon/__mock__/handlers.ts
@@ -2,7 +2,7 @@ import { http, HttpResponse } from 'msw'
 import { mockPokemonPickachu } from '@/domain/pokemon/__mock__/mockPokemon'
 
 export const pokemonHandlers = [
-  http.get('https://pokeapi.co/api/v2/pokemon/pickachu', ()=>{
+  http.get( process.env.HOSTNAME + '/api/proxy/pokemon/v2/pokemon/pickachu', ()=>{
     return HttpResponse.json(mockPokemonPickachu)
   })
 ]
