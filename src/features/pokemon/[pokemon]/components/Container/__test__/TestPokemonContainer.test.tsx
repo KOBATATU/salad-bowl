eeaa5f08e070e19d943b5ab3fe4a64b422e231bb
KOBATATU <kobatatu258@yahoo.co.jp>
2023-11-25 22:14:34 +0900

    update: fyndByを使うようにした

--- src/features/pokemon/[pokemon]/components/Container/__test__/TestPokemonContainer.test.tsx
+++ src/features/pokemon/[pokemon]/components/Container/__test__/TestPokemonContainer.test.tsx
@@ -1,6 +1,6 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { screen } from '@testing-library/dom'
-import { render, waitFor } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import { PokemonContainer } from '@/features/pokemon/[pokemon]/components/Container/PokemonContainer'
 
 const queryClient = new QueryClient()
@@ -9,9 +9,8 @@ describe('レンダリングテスト', () => {
     render(<QueryClientProvider client={queryClient}>
       <PokemonContainer params={{ pokemonName: 'pickachu' }} />
     </QueryClientProvider>)
-    await waitFor(() => {
-      expect(screen.getByText('25')).toBeInTheDocument()
-    })
+    expect(await screen.findByText('25')).toBeInTheDocument()
+    
     screen.debug()
   })
 })
