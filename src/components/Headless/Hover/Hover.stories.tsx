2abdde427a75a45d060bd1e146d87946d6e1f0dd
KOBATATU <kobatatu258@yahoo.co.jp>
2023-12-27 16:44:26 +0900

    update: hoverの位置関係を相対位置によって計算

--- src/components/Headless/Hover/Hover.stories.tsx
+++ src/components/Headless/Hover/Hover.stories.tsx
@@ -14,32 +14,37 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 const Menu = () => {
-  const { isHovered, hoverProps } = useHover()
+  const { isHovered, hoverProps } =
+    useHover<HTMLDivElement>({ type: 'right' })
 
   return (
-    <div className='p-1' {...hoverProps}>
+    <div className='relative p-1' >
       <ListItem
         prefixIcon={<MdDashboard className='text-white' />}
-        className='gap-3 hover:bg-gray-100 dark:hover:bg-gray-700'
+        className='relative gap-3 hover:bg-gray-100 dark:hover:bg-gray-700'
+        ref={hoverProps.buttonRef}
+        onMouseOut={hoverProps.onMouseOut}
+        onMouseOver={hoverProps.onMouseOver}
       >
         <a href="#">
           <Typography variant='span' color='white'>Dashboard</Typography>
         </a>
       </ListItem>
-      {isHovered && (
-        <div className='absolute w-full'
-          onMouseOver={hoverProps.onMouseOver}
-          onMouseOut={hoverProps.onMouseOut}
-          style={{
-            top: hoverProps.popoverPosition.top,
-            left:hoverProps.popoverPosition.width + hoverProps.popoverPosition.left
-          }}>
-          <List className='w-full rounded-lg border bg-white p-1'>
+      
+      <div className='z-100 absolute w-full rounded-lg border bg-white'
+        hidden={!isHovered}
+        onMouseOver={hoverProps.onMouseOver}
+        onMouseOut={hoverProps.onMouseOut}
+        ref={hoverProps.hoverRef}
+        style={hoverProps.hoverPosition}>
+        {isHovered && (
+          <List className='w-full '>
             <ListItem>通常のListItem</ListItem>
             <ListItem>通常のListItem</ListItem>
           </List>
-        </div>
-      )}
+        )}
+      </div>
+      
     </div>
 
   )
@@ -49,7 +54,7 @@ export const Hover:Story = {
   render: ()=>{
     return (
       <aside id="default-sidebar" className="fixed left-0 top-0 z-40 h-screen w-60 -translate-x-full transition-transform sm:translate-x-0" aria-label="Sidebar">
-        <div className="h-full overflow-y-auto bg-gray-50 py-4 dark:bg-gray-800">
+        <div className="h-full bg-gray-50 py-4 dark:bg-gray-800">
           <List className="space-y-2 p-0 font-medium">
             <Menu />
             <Menu />
--- src/hooks/useHover.ts
+++ src/hooks/useHover.ts
@@ -1,30 +1,52 @@
 import React from 'react'
+import { position } from '@/hooks/usePopover'
 
+type HoverProps = {
+  type: keyof position
+}
 
-export const useHover = () => {
+export const useHover = <T extends HTMLElement>(props: HoverProps) => {
+  const { type = 'bottom' } = props
   const [isHovered, setIsHovered] = React.useState(false)
-  const targetRef = React.useRef<HTMLDivElement>(null)
-  const [popoverPosition, setPopoverPosition] = React.useState({ left: 0, top: 0, width: 0,right:0 })
+  const buttonRef = React.useRef<T>(null)
+  const hoverRef = React.useRef<HTMLDivElement>(null)
+  const [hoverPosition, setHoverPosition] = 
+    React.useState<position>()
+  
+  const handleMouseOver = () => {
+    setIsHovered(true)
+  }
+  const handleMouseOut = () => {
+    setIsHovered(false)
+  }
 
+  React.useLayoutEffect(()=>{
+    const buttonNode = buttonRef.current
+    const popoverNode = hoverRef.current
 
-  const handleMouseOver = () => setIsHovered(true)
-  const handleMouseOut = () => setIsHovered(false)
+    if (isHovered && buttonNode && popoverNode) {
+      const marginPadding = getMarginPaddingInfo(buttonNode)
+      const { width, height } = popoverNode.getBoundingClientRect()
 
-  React.useEffect(()=>{
-    const node = targetRef.current
-    if (node) {
-      const rect = node.getBoundingClientRect()
-      setPopoverPosition({ left: rect.left, top: rect.top,width: rect.width,right: rect.right })
-
+      //相対値を計算
+      if (buttonRef.current.offsetParent) {
+        if(type === 'right') {
+          setHoverPosition({
+            left: buttonNode.offsetLeft + buttonNode.offsetWidth ,
+            top: buttonNode.offsetTop
+          })
+        }
+      }
     }
+  }, [isHovered, type])
 
-  },[targetRef ])
 
   const hoverProps = {
-    ref: targetRef,
+    buttonRef: buttonRef,
+    hoverRef: hoverRef,
     onMouseOver: handleMouseOver,
     onMouseOut: handleMouseOut,
-    popoverPosition: popoverPosition
+    hoverPosition: hoverPosition
   }
 
   return { isHovered, hoverProps }
